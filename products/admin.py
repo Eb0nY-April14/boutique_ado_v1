1fedc141382d3db0bfa7eaf2e446266a537808f6
Eb0nY-April14 <laratemi14@gmail.com>
2021-11-09 13:34:42 +0000

    Customized admin

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,33 @@ from django.contrib import admin
 from .models import Product, Category
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+
+class ProductAdmin(admin.ModelAdmin):
+    # The 'list display' attribute below is a tuple that tells
+    # the admin which fields to display. if we want to change the
+    # order of the columns in the admin, it can be adjusted here in
+    # the 'list display' attribute.
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    # We'll sort the products by 'SKU' using the ordering attribute
+    # since it's possible to sort on multiple columns. NOTE: This has
+    # to be a tuple even though it's only one field. To reverse it, just
+    # stick a minus in front of SKU.
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+# Here, we'll register our 2 new classes alongside their respective models.
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
--- products/models.py
+++ products/models.py
@@ -2,12 +2,22 @@ from django.db import models
 
 # Create your models here.
 
+# To fix the spelling issue of the 'category' model being spelt by django as
+# 'categorys', we'll add a special metaclass to the model itself & in the
+# metaclass, we'll specify 'verbose name plural' equals categories. This can
+# be done on any model to adjust the verbose name or the plural form of it from
+# the Django defaults & it won't require migration since no changes is made to
+# the structure of the model so this simple addition will fix that issue.
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
-    # We create a string method that takes in the category model itself & returns the
-    # category name
+    # We create a string method that takes in the category model itself &
+    # returns the category name
     def __str__(self):
         return self.name
 
@@ -16,23 +26,26 @@ class Category(models.Model):
         return self.friendly_name
 
 
-# The fields that are required in this Product model are name, description & price
-# while the rest are optional.
+# The fields that are required in this Product model are name, description &
+# price while the rest are optional.
 class Product(models.Model):
-    # The 1st field is a foreign key to the category model. We'll allow this to be null 
-    # in the database & blank in forms. If a category is deleted, we'll set any products 
-    # that use it to have null for this field rather than deleting the product.
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL )
+    # The 1st field is a foreign key to the category model. We'll allow this
+    # to be null in the database & blank in forms. If a category is deleted,
+    # we'll set any products that use it to have null for this field rather
+    # than deleting the product.
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    rating = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     has_sizes = models.BooleanField(null=True, blank=True, default=False)
 
-    #  The string method below will just return the products name 
+    #  The string method below will just return the products name
     def __str__(self):
         return self.name
 
