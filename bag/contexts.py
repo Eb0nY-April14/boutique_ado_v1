bd25b4a30bdd30efcb025872038a13a5d8b69886
Eb0nY-April14 <laratemi14@gmail.com>
2021-11-15 06:13:17 +0000

    Add the  add to bag functionality

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,6 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
 
+
 # We'll create a function called bag_contents which will take
 # request as a parameter & return a dictionary called context.
 def bag_contents(request):
@@ -9,16 +10,6 @@ def bag_contents(request):
     total = 0
     product_count = 0
 
-    # Since we want to give customers free delivery if they
-    # spend more than the amount specified in the free delivery
-    # threshold in settings.py, we'll use if statement to check
-    # for this.
-    # If total is less than the threshold, we'll calculate
-    # delivery cost as the total multiplied by the standard
-    # delivery percentage from settings.py which in this case
-    # is 10% but if If greater than or equal to the threshold,
-    # the else part kicks in where we'll set delivery & the
-    # free_delivery_delta to zero.
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         # The line of code after this comment lets the user know how
@@ -31,22 +22,6 @@ def bag_contents(request):
     # The grand total is calculated by adding the delivery charge to the total
     grand_total = delivery + total
 
-    # This is known as a context processor & its purpose is to make
-    # this dictionary available to all templates across the entire
-    # application. To make this context processor available to the
-    # entire application, we need to add it to the list of context
-    # processors in the templates variable in settings.py.
-    # This context used here is same as the ones we've been using
-    # in our views but the only difference is we're returning it
-    # directly and making it available to all templates by putting
-    # it in settings.py.
-
-    # The final step here is to add all these items i.e bag items,
-    # total, product count, delivery, free_delivery_delta,
-    # free_delivery_threshold & grand_total to the context so they'll
-    # be available in templates across the site. This means that
-    # everything in this context will be available in every template &
-    # app across the entire project
     context = {
         'bag_items': bag_items,
         'total': total,
--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -7,3 +7,47 @@ def view_bag(request):
     """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
+
+# The form we created in product_detail.html is submitted to this view
+def add_to_bag(request, item_id):
+    """ Add a quantity of the specified product to the shopping bag """
+
+    # On the next line below, we get the quantity from the form.
+    quantity = int(request.POST.get('quantity'))
+    # The code below this comment will get the redirect URL from the form
+    # so as to know where to redirect once the process here is finished.
+    redirect_url = request.POST.get('redirect_url')
+    # In modern versions of HTTP, every request-response cycle between
+    # the server & the client which in our case is between the django
+    # view on the server-side & our form making the request on the
+    # client-side uses a 'session' to allow information to be stored
+    # until the client & server are done communicating. This is very
+    # handy mostly in an e-commerce store as it allows us to store the
+    # contents of the shopping bag in the HTTP session while the user
+    # browses the site & adds items to be purchased. By storing the
+    # shopping bag in the session, they can add something to the bag,
+    # then browse to a different part of the site, add something else &
+    # so on without losing the contents of their bag. The session will
+    # persist until the user closes their browser. To implement this
+    # concept, we'll create a variable named bag which accesses the
+    # requests session & tries to get this variable if it already exists
+    # & if it doesn't, initializes it to an empty dictionary. It checks
+    # if there's a bag variable in the session & creates one if not.
+    # We'll get the  bag variable if it exists or create it if it doesn't
+    bag = request.session.get('bag', {})
+
+    # This will stuff the product into the dictionary we just created along
+    # with the quantity & will do so by creating a key of the items id & set
+    # it equal to the quantity. If the item is already in the bag i.e there's
+    # already a key in the bag dictionary matching this product id, then we'll
+    # increment its quantity rightly
+    if item_id in list(bag.keys()):
+        # This updates the item quantity if it already exists
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity  # This adds the item to the bag for the 1st time
+
+    # Here, we'll overwrite the variable in the session with the updated version.
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
